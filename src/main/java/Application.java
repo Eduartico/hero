cfae48931700e6444de2268a80b24753749752a6
Eduardo Duarte da Silva <up202004999@FEUPSIG.FE.UP.PT>
2021-11-05 16:00:47 +0000

    teste

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,19 +8,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-        TerminalSize terminalSize = new TerminalSize(200, 100);
-        DefaultTerminalFactory terminalFactory = new
-                DefaultTerminalFactory()
-                .setInitialTerminalSize(terminalSize);
-        Terminal terminal = terminalFactory.createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null);
-        screen.startScreen();
-        screen.doResizeIfNecessary();
-        screen.clear();
-        screen.setCharacter(10, 10,
-                TextCharacter.fromCharacter('X')[0]);
-        screen.refresh();
+    public static void main(String[] args){
+        Game jogo;
+        run(game)
     }
 }
