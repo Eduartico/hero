a5f1b3e09b383485d3ef6a7b846c64bde1488cb1
Eduartico <duarte.eduardoeds@gmail.com>
2021-11-12 12:21:20 -0300

    IT MOVES!

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -18,22 +19,39 @@ public class Game {
         screen.setCursorPosition(null); // we don't need a cursor
         screen.startScreen(); // screens must be started
         screen.doResizeIfNecessary(); // resize screen if necessary
-        TerminalSize terminalSize = new TerminalSize(x, y);
+        TerminalSize terminalSize = new TerminalSize(50, 25);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
     }
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(15, 15,
-                TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y,
+                TextCharacter.fromCharacter('O')[0]);
         screen.refresh();
     }
     public void run() throws IOException {
+        while(true){
         draw();
         KeyStroke key = screen.readInput();
         processKey(key);
+            if (key.getKeyType() == KeyType.Character &&
+                    key.getCharacter() == 'q' || key.getCharacter() == 'Q' ){
+                screen.close();
+            }
+        }
     }
     private void processKey(KeyStroke key) {
-        System.out.println(key);
+        if (key.getKeyType() == KeyType.ArrowUp){
+            y-=1;
+        };
+        if (key.getKeyType() == KeyType.ArrowDown){
+            y+=1;
+        };
+        if (key.getKeyType() == KeyType.ArrowLeft){
+            x-=1;
+        };
+        if (key.getKeyType() == KeyType.ArrowRight){
+            x+=1;
+        };
     }
 }
